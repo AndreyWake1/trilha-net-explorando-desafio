7241066292d029407cd9ac74b07e231bf589fdf2
andrey <andreyfellipeb@hotmail.com>
2025-08-26 21:25:52 -0300

    calculo_hospedes

--- Models/Reserva.cs
+++ Models/Reserva.cs
@@ -17,14 +17,18 @@ namespace DesafioProjetoHospedagem.Models
         {
             // TODO: Verificar se a capacidade é maior ou igual ao número de hóspedes sendo recebido
             // *IMPLEMENTE AQUI*
-            if (true)
+            if (Suite.Capacidade <= hospedes.Count)
             {
+                Console.WriteLine("Capacidade suficiente para os hóspedes.");
                 Hospedes = hospedes;
             }
+        
             else
             {
                 // TODO: Retornar uma exception caso a capacidade seja menor que o número de hóspedes recebido
                 // *IMPLEMENTE AQUI*
+                Console.WriteLine("A capacidade da suíte é menor que o número de hóspedes.");
+              
             }
         }
 
@@ -37,7 +41,11 @@ namespace DesafioProjetoHospedagem.Models
         {
             // TODO: Retorna a quantidade de hóspedes (propriedade Hospedes)
             // *IMPLEMENTE AQUI*
-            return 0;
+            if (Hospedes != null)
+            {
+                return Hospedes.Count;
+            }
+            return Hospedes.Count;
         }
 
         public decimal CalcularValorDiaria()
@@ -47,12 +55,17 @@ namespace DesafioProjetoHospedagem.Models
             // *IMPLEMENTE AQUI*
             decimal valor = 0;
 
+            valor = DiasReservados * Suite.ValorDiaria;
+
             // Regra: Caso os dias reservados forem maior ou igual a 10, conceder um desconto de 10%
             // *IMPLEMENTE AQUI*
-            if (true)
+            if (DiasReservados >= 10)
             {
-                valor = 0;
-            }
+                valor -= (valor * 0.10M);
+                Console.WriteLine("Desconto de 10% aplicado.");
+
+            }   
+          
 
             return valor;
         }
